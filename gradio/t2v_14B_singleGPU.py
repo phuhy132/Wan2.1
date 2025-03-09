f1b9c2a13bbd816d44c9b2da0f1bb8c319a3c940
phuhy132 <89736406+phuhy132@users.noreply.github.com>
2025-03-09 11:52:39 +0700

    Update t2v_14B_singleGPU.py

--- gradio/t2v_14B_singleGPU.py
+++ gradio/t2v_14B_singleGPU.py
@@ -202,4 +202,4 @@ if __name__ == '__main__':
     print("done", flush=True)
 
     demo = gradio_interface()
-    demo.launch(server_name="0.0.0.0", share=False, server_port=7860)
+    demo.launch(share=True)
